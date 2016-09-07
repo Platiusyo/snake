bdb418e0e618e70f2ad177df6e134a77b187e668
Kirin Mihail <MKirin@int.businesscar.ru>
2016-09-07 15:06:55 +0300

    -

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,25 +24,19 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            snake.Move();
-            System.Threading.Thread.Sleep(100);
-            Console.ReadLine();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(150);
+                snake.Move();
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                
+                
+            }
         }
     }
 }
