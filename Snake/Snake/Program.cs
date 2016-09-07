49b0673f4cd70a22a8918f1ee6cae9074820fb0e
Kirin Mihail <MKirin@int.businesscar.ru>
2016-09-07 12:58:51 +0300

    -

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            HorizontalLine uline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine dline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine lline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rline = new VerticalLine(0, 24, 78, '+');
+            uline.Draw();
+            dline.Draw();
+            lline.Draw();
+            rline.Draw();
 
-            HorizontalLine line = new HorizontalLine(5,10,8,'+');
-            line.Draw();
-
-            VerticalLine vline = new VerticalLine(5, 10, 10, '@');
-            vline.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }
